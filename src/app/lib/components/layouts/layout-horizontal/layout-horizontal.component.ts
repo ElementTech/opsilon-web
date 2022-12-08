e1c0d9d9018ca2c7bda39b00d90abcf957800c2a
Amitai Getzler <amitai.getzler@explorium.ai>
2022-12-08 12:48:11 +0200

    :lipstick: ui stuff
    modified:   src/app/app.component.html; modified:   src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.html; modified:   src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts; modified:   src/app/lib/components/logo/logo.component.html; modified:   src/app/lib/components/navbar/navbar.component.html; modified:   src/app/pages/home/home.page.html; modified:   src/app/pages/repositories/repositories.page.html

--- src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
+++ src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FooterComponent, NavbarComponent } from '@lib/components';
-
+import { LogoComponent } from '@lib/components/logo/logo.component';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-layout-horizontal',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, RouterModule,FooterComponent,LogoComponent],
   templateUrl: './layout-horizontal.component.html',
   styleUrls: ['./layout-horizontal.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
